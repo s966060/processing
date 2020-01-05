c13f64bae32ba5ac63a607621d2eeea2380880db
filip delbaen <filipdelbaen@msn.com>
2020-01-05 08:36:51 +0100

    outline for PApplets

---
+++ src/fde/processing/common/ProcessingOutline.java
@@ -0,0 +1,24 @@
+package fde.processing.common;
+
+import processing.core.PApplet;
+
+public class ProcessingOutline extends PApplet {
+    @Override
+    public void settings() {
+        super.settings();
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+    }
+
+    @Override
+    public void draw() {
+        super.draw();
+    }
+
+    public static void main(String[] args) {
+        PApplet.main(ProcessingOutline.class);
+    }
+}
