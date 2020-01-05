59d1ad396b7c09813d0b14a1c211caf80d9e44bc
filip delbaen <filipdelbaen@msn.com>
2020-01-05 09:14:28 +0100

    outline for PApplets

--- src/fde/processing/common/ProcessingOutline.java
+++ src/fde/processing/common/ProcessingOutline.java
@@ -18,6 +18,18 @@ public class ProcessingOutline extends PApplet {
         super.draw();
     }
 
+    public void red() {
+        stroke(color(255, 0, 0));
+    }
+
+    public void green() {
+        stroke(color(0, 255, 0));
+    }
+
+    public void blue() {
+        stroke(color(0, 0, 255));
+    }
+
     public static void main(String[] args) {
         PApplet.main(ProcessingOutline.class);
     }
